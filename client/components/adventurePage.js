ab175f998a109d0e5436b4beaf96c98b3f97de8e
devere-here <stevendeverehere@gmail.com>
2018-06-11 14:18:01 -0400

    feat: choosing an option generates new options and a new situation

--- client/components/adventurePage.js
+++ client/components/adventurePage.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import getNewOptions from '../store'
+import { getNewOptions, setSituation } from '../store'
 
 
 export class AdventurePage extends Component{
   constructor(props){
     super(props)
+  }
+
+  handleClick = (num) => {
+    console.log('this.props.situation.children[num]', this.props.situation.children[num])
+    this.props.getNextSiutation(this.props.situation.children[num])
+    this.props.getNextSituationOptions(this.props.option)
   }
 
   render = () => {
@@ -14,15 +20,16 @@ export class AdventurePage extends Component{
 
       <div>
         <h1>In Adventure Page</h1>
-        {this.props.option.length === 0
+        {this.props.option.length === 0 || Object.keys(this.props.situation).length === 0
         ? null
         : (
           <div>
             <h2>{this.props.situation.value.situation}</h2>
-            <button>{this.props.option[0].option}</button>
-            <button>{this.props.option[1].option}</button>
-            <button>{this.props.option[2].option}</button>
-            <button>{this.props.option[3].option}</button>
+            <h2>What do you do?</h2>
+            <button onClick={() => this.handleClick(0)}>{this.props.option[0].option}</button>
+            <button onClick={() => this.handleClick(1)}>{this.props.option[1].option}</button>
+            <button onClick={() => this.handleClick(2)}>{this.props.option[2].option}</button>
+            <button onClick={() => this.handleClick(3)}>{this.props.option[3].option}</button>
           </div>
         )}
       </div>
@@ -40,9 +47,13 @@ const mapState = ({situation, option}) => ({
 })
 
 const mapDispatch = (dispatch) => () => ({
-  nextSituationOptions(){
-    dispatch(getNewOptions())
+  getNextSituationOptions(options){
+    dispatch(getNewOptions(options))
+  },
+  getNextSiutation(situation){
+    dispatch(setSituation(situation))
   }
+
 })
 
 export default connect(mapState, mapDispatch)(AdventurePage)
