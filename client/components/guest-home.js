7cd5c974c6c381785dfa68e6cca3585c848110bb
devere-here <stevendeverehere@gmail.com>
2018-06-10 23:39:11 -0400

    feat: created guestHome that contains a button that seeds our redux store

---
+++ client/components/guest-home.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import { setIntroOptions, setIntroSituation } from '../store'
+
+/**
+ * COMPONENT
+ */
+class GuestHome extends Component{
+  constructor(props){
+    super(props)
+  }
+
+  handleClick = () => {
+    this.props.setUpSituationTree()
+    this.props.setUpOptions()
+    this.props.history.push(`/adventure`)
+  }
+
+  render = () => {
+    return (
+      <div>
+        <h3>Welcome Guest</h3>
+        <h4>Press the button below to begin your adventure</h4>
+        <button type='submit' onClick={() => this.handleClick()}>Begin Adventure</button>
+      </div>
+    )
+  }
+}
+
+/**
+ * CONTAINER
+ */
+
+const mapDispatch = (dispatch) => ({
+  setUpSituationTree() {
+    dispatch(setIntroSituation())
+  },
+  setUpOptions() {
+    dispatch(setIntroOptions())
+  }
+
+})
+
+
+export default connect(null, mapDispatch)(GuestHome)
